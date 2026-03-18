4b8f4c76570a96a2e1d9531d00943341ca2b24de
Gabriel <tu-email@example.com>
2026-03-18 15:18:15 -0500

    Fourth: Datos de enlace remoto añadidos a correo de confirmación.

--- wsHelpdesk/target/generated-sources/annotations/com/asotec/riesgos/entity/SgfEstadoTitulo_.java
+++ wsHelpdesk/target/generated-sources/annotations/com/asotec/riesgos/entity/SgfEstadoTitulo_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2026-03-16T09:41:53")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2026-03-18T15:06:38")
 @StaticMetamodel(SgfEstadoTitulo.class)
 public class SgfEstadoTitulo_ { 
 
