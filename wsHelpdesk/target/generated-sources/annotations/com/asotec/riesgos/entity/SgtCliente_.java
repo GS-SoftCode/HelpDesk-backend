5174779507316d72ad8e64d5998fbddc7de7324b
Gabriel <tu-email@example.com>
2026-03-27 15:33:17 -0500

    6°: Variables de entorno listas.

--- wsHelpdesk/target/generated-sources/annotations/com/asotec/riesgos/entity/SgtCliente_.java
+++ wsHelpdesk/target/generated-sources/annotations/com/asotec/riesgos/entity/SgtCliente_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2026-03-27T15:29:04")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2026-03-27T15:32:25")
 @StaticMetamodel(SgtCliente.class)
 public class SgtCliente_ { 
 
--- wsHelpdesk/target/generated-sources/annotations/com/asotec/riesgos/entity/SgtTecnico_.java
+++ wsHelpdesk/target/generated-sources/annotations/com/asotec/riesgos/entity/SgtTecnico_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2026-03-27T15:29:04")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2026-03-27T15:32:25")
 @StaticMetamodel(SgtTecnico.class)
 public class SgtTecnico_ { 
 
